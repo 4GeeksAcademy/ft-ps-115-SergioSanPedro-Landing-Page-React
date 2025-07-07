c67f303b61cddd4b9526dabd843182e583e957e3
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-07-07 10:07:59 +0000

    navbar acabado

---
+++ src/js/components/NavBar.jsx
@@ -0,0 +1,28 @@
+export const NavBar = () => {
+
+    return (
+    <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container-fluid mx-4">
+            <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+            <button className="navbar-toggler navbar-dark bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
+                    <li className="nav-item">
+                        <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled text-light" aria-disabled="true">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled text-light" aria-disabled="true">Services</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled text-light" aria-disabled="true">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+)}
