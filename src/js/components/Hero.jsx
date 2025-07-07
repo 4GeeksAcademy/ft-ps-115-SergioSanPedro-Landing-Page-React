f508806084682fe23f10b98684d34aeff4e3afef
Sergio San Pedro Álvarez <sergiospalvarez@hotmail.com>
2025-07-07 12:52:50 +0000

    save2

--- src/js/components/Hero.jsx
+++ src/js/components/Hero.jsx
@@ -1,14 +1,14 @@
 export const Hero = () => {
 
     return (
-        <div className="px-4 py-5 my-5 d-flex justify-content-start bg-light">
-            <h1 className="display-5 fw-bold">TOP cartas Magic</h1>
+        <div className="px-4 py-5 my-5 d-flex justify-content-start bg-white bg-opacity-25" style={{textShadow: "2px 3px 4px rgba(0,0,0,0.8)"}}>
+            <h1 className="display-5 fw-bold text-white">TOP cartas Magic</h1>
             <div className="p-2 col-md-4 mx-auto">
-                <p className="lead mb-4">
+                <p className="lead mb-4 fw-bold text-white" >
                     Bienvenida a la página donde podrás encontrar las mejores cartas Magic que podrás
-                     utilizar en tus decks y así poder llegar a conseguir llegar a lo más alto. Sólo te voy a mostrar el top 5 de las cartas y sus precios, 
+                     utilizar en tus decks y así poder llegar a conseguir llegar a lo más alto. Sólo te voy a mostrar el top 4 de las cartas y sus precios, 
                      Hay muchísimas más y cada una irá bien dependiendo de la temática en la que quieras jugar y estrategia, pero te muestro las estándares que pueden 
-                     servir para todos los tipos deck´s. Mucha suerte!!
+                     servir para todos los tipos deck`s. Mucha suerte!!
                 </p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                     <button type="button" className="btn btn-primary btn-md px-4">
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -19,29 +19,36 @@ const Home = () => {
 					<div className="col-lg-3 col-md-6">
 						<Cards
 							imagen='la imagen'
-							titulo='Nombre de la carta'
-							descripcion='descripcion de la carta'
+							titulo='Black Lotus'
+							descripcion='Esta carta es la más icónica y poderosa de Magic. 
+							Permite generar tres manás de cualquier color sin coste, 
+							lo que puede ser decisivo en los primeros turnos para establecer una ventaja abrumadora. '
 						/>
 					</div>
 					<div className="col-lg-3 col-md-6">
 						<Cards
 							imagen='la imagen'
-							titulo='Nombre de la carta'
-							descripcion='descripcion de la carta'
+							titulo='Ancestral Recall'
+							descripcion='Una carta que roba tres cartas, 
+							es una herramienta esencial para mazos que buscan acumular 
+							recursos y opciones estratégicas. '
 						/>
 					</div>
 					<div className="col-lg-3 col-md-6">
 						<Cards
 							imagen='la imagen'
-							titulo='Nombre de la carta'
-							descripcion='descripcion de la carta'
+							titulo='Time Walk'
+							descripcion='Permite al jugador tomar un turno adicional, 
+							lo que puede romper el ritmo del oponente y 
+							generar un impacto significativo en el desarrollo de la partida. '
 						/>
 					</div>
 					<div className="col-lg-3 col-md-6">
 						<Cards
 							imagen='la imagen'
-							titulo='Nombre de la carta'
-							descripcion='descripcion de la carta'
+							titulo='Mox Sapphire'
+							descripcion='Una de las cinco cartas "Mox", esta criatura permite generar maná azul, 
+							lo que facilita el despliegue de hechizos y criaturas de este color, muy comunes en mazos competitivos. '
 						/>
 					</div>
 					
